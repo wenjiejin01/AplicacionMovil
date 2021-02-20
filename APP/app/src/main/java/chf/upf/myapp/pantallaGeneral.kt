ec4452b75ae23bfb2e2e83a892a7a7ef12133cce
Hortensiusc <37106102+Hortensiusc@users.noreply.github.com>
2021-02-20 11:03:07 +0100

    APP
    Proyecto Aplicacion

---
+++ APP/app/src/main/java/chf/upf/myapp/pantallaGeneral.kt
@@ -0,0 +1,11 @@
+package chf.upf.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pantallaGeneral : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_general)
+    }
+}
