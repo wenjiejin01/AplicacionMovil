0c06feb5c0219f70626c47d11da25fccd132f958
Hortensiusc <37106102+Hortensiusc@users.noreply.github.com>
2021-02-22 17:36:09 +0100

    LOGIN Y REGISTRO
    Practicamente acabada la parte de Login y Registro, bastante falseada con ifs, si da tiempo mas adelante la podemos hacer con la base de datos

--- APP/app/src/main/java/chf/upf/myapp/MainActivity.kt
+++ APP/app/src/main/java/chf/upf/myapp/MainActivity.kt
@@ -1,11 +1,52 @@
 package chf.upf.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val userInputField = findViewById<EditText>(R.id.userText)
+        val passwordInputField = findViewById<EditText>(R.id.passwordText)
+
+        // Variables para falsear el inicio de sesion
+        val username = "aplicacion"
+        val password = "contraseña"
+
+        val lButton = findViewById<Button>(R.id.loginButton)
+
+
+        lButton.setOnClickListener {
+            if (userInputField.text.toString().equals(username)){
+                if (passwordInputField.text.toString().equals(password)){
+                    val intentLogin = Intent(this, pantallaGeneral::class.java).apply {}
+                    startActivity(intentLogin)
+                }else {
+                    Toast.makeText(this, "El usuario o la contraseña son incorrectas", Toast.LENGTH_SHORT).show()
+                }
+            } else{
+                Toast.makeText(this, "El usuario o la contraseña son incorrectas", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val rButton = findViewById<Button>(R.id.registerButton)
+        rButton.setOnClickListener {
+            val intentRegister = Intent(this, registerActivity::class.java).apply {}
+            startActivity(intentRegister)
+        }
+
+
+
+
+
     }
+
 }
