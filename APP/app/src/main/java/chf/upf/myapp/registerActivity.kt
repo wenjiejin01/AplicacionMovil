6c5b472e3bec2aa71e76aa405f7713d35dba1a29
Hortensiusc <37106102+Hortensiusc@users.noreply.github.com>
2021-02-28 18:26:25 +0100

    Registro
    Pantalla registro terminada

--- APP/app/src/main/java/chf/upf/myapp/registerActivity.kt
+++ APP/app/src/main/java/chf/upf/myapp/registerActivity.kt
@@ -1,11 +1,21 @@
 package chf.upf.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class registerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+        val rdButton = findViewById<Button>(R.id.registeredButton)
+        rdButton.setOnClickListener {
+            val backMain = Intent(this, MainActivity::class.java).apply {}
+            startActivity(backMain)
+        }
+
     }
 }
