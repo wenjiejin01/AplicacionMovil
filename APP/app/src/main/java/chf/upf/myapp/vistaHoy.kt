33f8c15f15ce99c0629f11aeaf33e9b930290411
virgimarine <66692337+virgimarine@users.noreply.github.com>
2021-03-01 01:53:26 +0100

    Create vistaHoy.kt

---
+++ APP/app/src/main/java/chf/upf/myapp/vistaHoy.kt
@@ -0,0 +1,11 @@
+package chf.upf.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class vistaHoy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.vista_hoy)
+    }
+}
