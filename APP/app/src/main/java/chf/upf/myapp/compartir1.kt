025b7d2a763e5f67a2a5901ddbb843cdf21c1bd3
virgimarine <66692337+virgimarine@users.noreply.github.com>
2021-03-01 01:53:21 +0100

    Create compartir1.kt

---
+++ APP/app/src/main/java/chf/upf/myapp/compartir1.kt
@@ -0,0 +1,11 @@
+package chf.upf.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class compartir1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.compartir1)
+    }
+}
