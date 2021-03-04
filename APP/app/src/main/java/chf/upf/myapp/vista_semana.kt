03f3360561e35f1b42918782967665d02d6bd876
Hortensiusc <37106102+Hortensiusc@users.noreply.github.com>
2021-03-04 10:59:22 +0100

    Reunion 04/03

--- APP/app/src/main/java/chf/upf/myapp/vista_semana.kt
+++ APP/app/src/main/java/chf/upf/myapp/vista_semana.kt
@@ -2,10 +2,19 @@ package chf.upf.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class vista_semana : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vista_semana)
+
+
+        val calendarButton = findViewById<FloatingActionButton>(R.id.floatingActionButton4)
+
+
+
+
+
     }
 }
