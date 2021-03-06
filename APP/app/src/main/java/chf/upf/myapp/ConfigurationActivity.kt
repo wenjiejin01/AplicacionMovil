ca13dc9044ad341bd1f59a2c3a8c86b56de8a59b
Hortensiusc <37106102+Hortensiusc@users.noreply.github.com>
2021-03-06 13:54:52 +0100

    configuracion

--- APP/app/src/main/java/chf/upf/myapp/ConfigurationActivity.kt
+++ APP/app/src/main/java/chf/upf/myapp/ConfigurationActivity.kt
@@ -1,11 +1,21 @@
 package chf.upf.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ConfigurationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuration)
+
+
+        val closeSession = findViewById<Button>(R.id.closeSessionButton)
+        closeSession.setOnClickListener {
+            val intentCloseSession = Intent(this, MainActivity::class.java).apply {  }
+            startActivity(intentCloseSession)
+        }
+
     }
 }
