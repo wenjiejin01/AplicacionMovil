7759805f903c69c87b42e95bc299b88ffd758c0b
Wenjie_Jin <32678710+daw2-Jin18@users.noreply.github.com>
2021-03-12 10:56:48 +0100

    Premium 1.1

--- APP/app/src/main/java/chf/upf/myapp/Pagament.kt
+++ APP/app/src/main/java/chf/upf/myapp/Pagament.kt
@@ -1,13 +1,18 @@
 package chf.upf.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Pagament : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pagament)
 
-
+        findViewById<Button>(R.id.botonPago).setOnClickListener{
+            val intentP = Intent(this, study_tech_premium::class.java).apply {  }
+            startActivity(intentP)
+        }
     }
 }
--- APP/app/src/main/java/chf/upf/myapp/study_tech_premium.kt
+++ APP/app/src/main/java/chf/upf/myapp/study_tech_premium.kt
@@ -2,10 +2,13 @@ package chf.upf.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class study_tech_premium : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_study_tech_premium)
+
+        Toast.makeText(this, "Bienvenido a Time to Start Premium!!!", Toast.LENGTH_SHORT).show()
     }
 }
