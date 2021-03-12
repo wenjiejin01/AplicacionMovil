5b5e28e17a00943160f217ad1a556b2efe9722d0
Wenjie_Jin <32678710+daw2-Jin18@users.noreply.github.com>
2021-03-12 10:29:21 +0100

    view pagament

---
+++ APP/app/src/main/java/chf/upf/myapp/Pagament.kt
@@ -0,0 +1,11 @@
+package chf.upf.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pagament : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagament)
+    }
+}
