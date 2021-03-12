880dac86fd2ca5aabfd3b9174b104aa5816324d6
Hortensiusc <37106102+Hortensiusc@users.noreply.github.com>
2021-03-12 10:17:51 +0100

    Estadisticas
    Añadidas mas estadisticas, corregido algunos errores

--- APP/app/src/main/java/chf/upf/myapp/StudyTech.kt
+++ APP/app/src/main/java/chf/upf/myapp/StudyTech.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class StudyTech : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +14,20 @@ class StudyTech : AppCompatActivity() {
         findViewById<Button>(R.id.botonPomodoro).setOnClickListener {
             val intentP = Intent(this, PomodoroActivity::class.java).apply {  }
             startActivity(intentP)
+        }
+
+        findViewById<Button>(R.id.botonTecnica2).setOnClickListener {
+            Toast.makeText(this, "Debes de ser usuario Premium para acceder " +
+                    "a esta funcionalidad", Toast.LENGTH_SHORT).show()
+
+            // añadir el intent para acceder a la activiadad de comprar version premium
+        }
+
+        findViewById<Button>(R.id.botonTecnica3).setOnClickListener {
+            Toast.makeText(this, "Debes de ser usuario Premium para acceder " +
+                    "a esta funcionalidad", Toast.LENGTH_SHORT).show()
+
+            // añadir el intent para acceder a la activiadad de comprar version premium
         }
     }
 }
